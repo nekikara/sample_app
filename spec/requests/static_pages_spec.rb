bc13589ed812e78336fdabe6a453f858b1ebf8d0
u_jigawa <ujigawa.damu@gmail.com>
2014-11-03 00:16:43 +0900

    Add a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,10 +8,14 @@ describe "StaticPages" do
       expect(page).to have_content('Sample App')
     end
 
-    it "should have the title 'Home'" do
-      # run the generator again with the --webrat flag if you want to use webrat methods/matchers
+    it "should have the base title" do
       visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title("| Home")
     end
   end
 
@@ -25,7 +29,7 @@ describe "StaticPages" do
     it "should have the title 'Help'" do
       # run the generator again with the --webrat flag if you want to use webrat methods/matchers
       visit '/static_pages/help'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+      expect(page).to have_title("| Help")
     end
   end
   describe "About page" do
@@ -38,7 +42,20 @@ describe "StaticPages" do
     it "should have the title 'About Us'" do
       # run the generator again with the --webrat flag if you want to use webrat methods/matchers
       visit '/static_pages/about'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+      expect(page).to have_title("| About Us")
+    end
+  end
+  describe "Contact page" do
+    it "should have the content 'Contact'" do
+      # run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      # run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      visit '/static_pages/contact'
+      expect(page).to have_title("| Contact")
     end
   end
 end
